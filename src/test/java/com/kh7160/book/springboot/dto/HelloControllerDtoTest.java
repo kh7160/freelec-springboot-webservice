64f6524077adb481abfc580484ad5c3fbc935d6f
jangkh <kh7160@naver.com>
2021-01-11 20:00:39 +0900

    20210110

---
+++ src/test/java/com/kh7160/book/springboot/dto/HelloControllerDtoTest.java
@@ -0,0 +1,22 @@
+package com.kh7160.book.springboot.dto;
+
+import com.kh7160.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloControllerDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
