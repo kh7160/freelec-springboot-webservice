422c8e9ea59ac3a6e1032bd7d19a40c69aafe956
jangkh <kh7160@naver.com>
2021-01-15 22:04:48 +0900

    20210114

--- src/main/java/com/kh7160/book/springboot/web/IndexController.java
+++ src/main/java/com/kh7160/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kh7160.book.springboot.web;
 
+import com.kh7160.book.springboot.config.auth.LoginUser;
+import com.kh7160.book.springboot.config.auth.dto.SessionUser;
 import com.kh7160.book.springboot.service.posts.PostsService;
 import com.kh7160.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,18 +10,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
 //    @GetMapping("/")
-//    public String index() {
+//    public String index(Model model) {
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        if (user != null) {
+//            model.addAttribute("userName", user.getName());
+//        }
+//
 //        return "index";
 //    }
-    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
